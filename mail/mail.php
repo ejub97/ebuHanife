08309be14f1a15dd94bac4247e9cefff787e9ebf
ejub hasanovic <ejubhasanovic@ejubs-MacBook-Pro.local>
2024-12-16 21:21:11 +0100

    Prijava

--- mail/mail.php
+++ mail/mail.php
@@ -1,22 +1,36 @@
-<?
-$name = $_POST{'name'};
-$email = $_POST{'email'};
-$phone = $_POST{'phone'};
-$subject = $_POST{'subject'};
-$message = $_POST['message'];
+<?php
+// Dohvati podatke iz POST zahtjeva i sanitiziraj ih
+$name = htmlspecialchars(trim($_POST['name']));
+$email = htmlspecialchars(trim($_POST['email']));
+$phone = htmlspecialchars(trim($_POST['phone']));
+$subject = htmlspecialchars(trim($_POST['subject']));
+$message = htmlspecialchars(trim($_POST['message']));
 
+// Sastavi sadržaj poruke
 $email_message = "
-
-Name: ".$name."
-Email: ".$email."
-Phone: ".$phone."
-Subject: ".$subject."
-Message: ".$message."
-
+Name: $name\n
+Email: $email\n
+Phone: $phone\n
+Subject: $subject\n
+Message: $message\n
 ";
 
-mail ("name@youremail.com" , "New Message", $email_message);
-header("location: ../mail-success.html");
+// Definiraj e-mail primatelja
+$to = "school.ebuhanife@gmail.com"; // Tvoja e-mail adresa
+$email_subject = "Nova poruka sa kontakt forme";
+
+// Postavi zaglavlja (headers)
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+// Pošalji e-mail
+if (mail($to, $email_subject, $email_message, $headers)) {
+    // Ako je uspješno poslan, preusmjeri na uspjeh stranicu
+    header("Location: ../mail-success.html");
+    exit();
+} else {
+    // Ako nije uspjelo, prikaži poruku o grešci
+    echo "Greška prilikom slanja poruke. Pokušajte ponovo.";
+}
 ?>
-
-
